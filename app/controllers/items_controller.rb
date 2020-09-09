b288557fd4a42909948f0481f46dc1807d7e6b8c
Naohiro Mitsukuchi <bump11nao@gmail.com>
2020-09-09 16:19:48 +0900

    addiction routes and action
    Items controllerに
    商品出品ページへのnewアクション
    と保存するcreateアクションを記述した。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,17 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create  
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :introduction, :price,:category, :size, :brand,:item_condition, :postage_payer, :prefecture_code, :preparation_day, :status).merge(user: current_user.id)
+  end
 end
