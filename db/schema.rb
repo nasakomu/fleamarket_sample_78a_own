e47a4d1c34b83a3c0355d9efbe4cc2593fea10ac
nasakomu <nasakomunasakomu@gmail.com>
2020-09-13 08:10:34 +0900

    setup ancestry

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_12_225440) do
+ActiveRecord::Schema.define(version: 2020_09_12_230029) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "ancestry"
+    t.index ["ancestry"], name: "index_categories_on_ancestry"
   end
 
 end
