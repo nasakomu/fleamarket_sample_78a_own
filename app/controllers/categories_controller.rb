d7cb75dbe5ca876f45fd9e9060ccf244bd3a379d
nasakomu <nasakomunasakomu@gmail.com>
2020-09-16 08:15:47 +0900

    create routing and controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,12 @@
 class CategoriesController < ApplicationController
+
+  def index
+    @categories = Category.where(ancestry: nil)
+  end
+
+  def show
+    @category = Category.find(params[:id])
+    # @items = @category.items
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
       get 'top'
     end
   end
+  resources :categories, only: [:index, :show]
 end
