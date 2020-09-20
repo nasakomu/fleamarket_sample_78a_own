0d91c89813854f2a00cb2d4a28a4f7fb96283913
Naohiro Mitsukuchi <bump11nao@gmail.com>
2020-09-20 17:12:27 +0900

    set validation

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,26 +6,25 @@ class Item < ApplicationRecord
 
   belongs_to :category
   belongs_to_active_hash :size
-  belongs_to :brand
+  belongs_to :brand, optional: true
   belongs_to_active_hash :item_condition
   belongs_to_active_hash :postage_payer
   belongs_to_active_hash :preparation_day
   belongs_to_active_hash :status
   # belongs_to :user
 
-  # with_options presence: true do
-  #   validates :name
-  #   validates :introduction
-  #   validates :price
-  #   validates :category
+  with_options presence: true do
+    validates :name
+    validates :introduction
+    validates :price
+    validates :category
 
-    # with_options numericality: { other_than: 1 } do
-    #   validates :size
-    #   validates :item_condition
-    #   validates :postage_payer
-    #   validates :prefecture_code
-    #   validates :preparation_day
-    # end
-  # end
-
+    with_options numericality: { other_than: 1 } do
+      validates :size_id
+      validates :item_condition_id
+      validates :postage_payer_id
+      validates :prefecture_code
+      validates :preparation_day_id
+    end
+  end
 end
