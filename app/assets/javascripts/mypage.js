fa48ad10b72d39502e16420ac262ddd42accf53b
Naohiro Mitsukuchi <bump11nao@gmail.com>
2020-10-02 17:02:20 +0900

    put temporary file
    一時的に必要と思われるファイルを作成

--- app/assets/javascripts/mypage.js
+++ app/assets/javascripts/mypage.js
@@ -1,17 +1,25 @@
 $(document).on('turbolinks:load', ()=>{
-  $(".MyPage .SideBar .Nav__content").hover(
-    function(){
+  $(".MyPage .SideBar .Nav__content").on({
+    'mouseenter': function(){
       let icon =  $(this).find("i")
       $(this).css('background-color', '#fafafa');
       icon.css('right', '10px');
       icon.css('font-size', '24px');
       icon.css('color', '#555');
     },
-    function(){
+    'mouseleave': function(){
       let icon =  $(this).find("i")
       $(this).css('background-color', '#fff');
       icon.css('right', '15px');
       icon.css('font-size', '20px')
       icon.css('color', '#ccc');
+    },
+    'click': function(){
+      let icon =  $(this).find("i")
+      $(this).css('background-color', '#fff');
+      icon.css('right', '15px');
+      icon.css('font-size', '20px')
+      icon.css('color', '#ccc');
+    }
   });
 })
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
   root 'items#top'
-  resources :items, only: [:index, :new, :create] do
+  resources :items, only: [:index, :new, :create, :show] do
     collection do
       get 'top'
     end
   end
+  # userコントローラに配置する
+  # get 'putting_up_list', on: :show
+  # get 'complicated_item', on: :show
+  # get 'profile', on: :edit
+  # get 'destination', on: :edit
+  # get 'payment_method', on: :edit
   resources :categories, only: [:index, :show] do
     collection do
       get 'get_category_children', defaults: { format: 'json' }
