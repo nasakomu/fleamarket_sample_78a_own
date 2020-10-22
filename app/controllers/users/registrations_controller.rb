4e39d1f7fd24e6036e388e977a169833bdcab163
Naohiro Mitsukuchi <bump11nao@gmail.com>
2020-10-22 22:36:57 +0900

    edit user/edit file

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  
-
+  before_action :set_current_user, only: [:update, :edit]
+  # before_action :configure_account_update_params, only: [:update], if: :devise_controller?
 
   def new
     @user = User.new
@@ -17,15 +17,26 @@ class Users::RegistrationsController < Devise::RegistrationsController
     @user.save
   end
 
-  
-  # private
-  
-
-  # def user_params
-  #   params.require(:user).permit(:nickname,:email,:encrypted_password, profile_attributes:[:first_name,:family_name, :first_name_kana,:family_name_kana,:birth_date])
+  # def update
+  #   binding.pry
+  #   if @user.update(configure_account_update_params)
+  #     redirect_to root_path
+  #   else
+  #     render :edit
+  #   end
   # end
-  # protected
 
+  private
+  
+  def set_current_user
+    @user = User.find(current_user.id)
+  end
+  
+  protected
+  
+  # def configure_account_update_params
+  #   devise_parameter_sanitizer.permit(:account_update,keys: [:nickname,:email,:encrypted_password, profile_attributes:[:first_name,:family_name, :first_name_kana,:family_name_kana,:birth_date],destination_attributes:[:first_name,:family_name,:first_name_kana,:family_name_kana,:postal_code,:prefecture_code,:city, :house_number,:room_number,:phone_number]])
+  # end
   # def after_sign_up_path_for(resource)
   #   # サインアップ後のリダイレクト先をrootに変更
   #   new_destination_path
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,10 +14,25 @@ class User < ApplicationRecord
   include JpPrefecture
   jp_prefecture :prefecture_code
   has_many :items, dependent: :destroy
+  
   def prefecture_name
     JpPrefecture::Prefecture.find(code: prefecture_code).try(:name)
   end
+
   def prefecture_name=(prefecture_name)
     self.prefecture_code = JpPrefecture::Prefecture.find(name: prefecture_name).code
+  end
+
+  def update_without_current_password(params, *options)
+    params.delete(:current_password)
+
+    if params[:password].blank? && params[:password_confirmation].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation)
+    end
+
+    result = update_attributes(params, *options)
+    clean_up_passwords
+    result
   end
 end
