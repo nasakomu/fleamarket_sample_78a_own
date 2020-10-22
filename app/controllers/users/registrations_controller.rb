0d1d8a0709af3c90c79cd35da32f68c77e1d1e41
Naohiro Mitsukuchi <bump11nao@gmail.com>
2020-10-22 23:56:18 +0900

    able to edit user info
    caution) invalid without password

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :set_current_user, only: [:update, :edit]
-  # before_action :configure_account_update_params, only: [:update], if: :devise_controller?
 
   def new
     @user = User.new
@@ -16,35 +14,17 @@ class Users::RegistrationsController < Devise::RegistrationsController
     @user = User.new(configure_sign_up_params)
     @user.save
   end
-
-  # def update
-  #   binding.pry
-  #   if @user.update(configure_account_update_params)
-  #     redirect_to root_path
-  #   else
-  #     render :edit
-  #   end
-  # end
-
-  private
-  
-  def set_current_user
-    @user = User.find(current_user.id)
-  end
   
   protected
-  
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update,keys: [:nickname,:email,:encrypted_password, profile_attributes:[:first_name,:family_name, :first_name_kana,:family_name_kana,:birth_date],destination_attributes:[:first_name,:family_name,:first_name_kana,:family_name_kana,:postal_code,:prefecture_code,:city, :house_number,:room_number,:phone_number]])
-  # end
+
   # def after_sign_up_path_for(resource)
   #   # サインアップ後のリダイレクト先をrootに変更
   #   new_destination_path
   # end
 
-  # def after_update_path_for(resource)
-  #   user_path(current_user)
-  # end
+  def after_update_path_for(resource)
+    users_show_path(current_user)
+  end
 
   # def update_resource(resource, params)
   #   resource.update_without_password(params)
