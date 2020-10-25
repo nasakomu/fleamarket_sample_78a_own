c4ddd71deee4752cdc404d24a046c0269faa1fe7
Naohiro Mitsukuchi <bump11nao@gmail.com>
2020-10-25 20:58:56 +0900

    modify Destination and Profile controller

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,11 +1,11 @@
 class DestinationsController < ApplicationController
+  before_action :correct_user
+  before_action :set_destination
 
   def edit
-    @destination = current_user.destination
   end
 
   def update
-    @destination = current_user.destination
     if @destination.update(destination_params)
       redirect_to users_show_path, notice: "情報を変更しました"
     else
@@ -17,5 +17,14 @@ class DestinationsController < ApplicationController
 
   def destination_params
     params.require(:destination).permit(:first_name,:family_name,:first_name_kana,:family_name_kana,:postal_code,:prefecture_code,:city, :house_number,:room_number,:phone_number).merge(user_id: current_user.id)
+  end
+
+  def set_destination
+    @destination = current_user.destination
+  end
+
+  def correct_user
+    correct_user = Destination.find(params[:id]).user
+    redirect_to(root_path) unless correct_user == current_user && user_signed_in?
   end
 end
--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,11 +1,11 @@
 class ProfilesController < ApplicationController
+  before_action :correct_user
+  before_action :set_profile
 
   def edit
-    @profile = current_user.profile
   end
 
   def update
-    @profile = current_user.profile
     if @profile.update(profile_params)
       redirect_to users_show_path, notice: "情報を変更しました"
     else
@@ -17,5 +17,14 @@ class ProfilesController < ApplicationController
 
   def profile_params
     params.require(:profile).permit(:first_name,:family_name, :first_name_kana,:family_name_kana,:birth_date).merge(user_id: current_user.id)
+  end
+
+  def set_profile
+    @profile = current_user.profile
+  end
+
+  def correct_user
+    correct_user = Profile.find(params[:id]).user
+    redirect_to(root_path) unless correct_user == current_user && user_signed_in?
   end
 end
